ab89262a5395619c9ed84f570c49e4b0f1a2efff
annalia01 <annalia.ruggiero01@gmail.com>
2025-10-02 09:29:57 +0200

    Add files via upload

---
+++ esempi/SpMV/inc/dataset8.h
@@ -0,0 +1,10 @@
+#define R 4
+#define C 5
+#define NNZ 7
+
+
+const uint8_t val[7]    = {5, 1, 2, 3, 4, 7, 6};
+const uint8_t idx[7]    = {2, 4, 1, 3, 0, 3, 4};
+const uint8_t ptr[5]    = {0, 2, 4, 5, 7};
+const uint8_t x[5]      = {1, 2, 3, 4, 5};
+const uint8_t verify_data[4] = {20, 16, 4, 58};
